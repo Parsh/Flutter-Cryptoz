bdc549432cb939d66ec9d8c808a8e49073eb2aaf
Parsh-Dev <parsh.cosmos11@gmail.com>
2018-03-11 19:57:39 +0530

    Version 0.2

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -15,20 +15,58 @@ class _HomePageState extends State<HomePage> {
 
   List currencies;
   int number;
-  final List<MaterialColor> colors = [Colors.blue, Colors.red, Colors.indigo,]
+  final List<MaterialColor> colors = [Colors.blue, Colors.red, Colors.indigo];
 
   Future<List> getCurrencies() async{
     number = 50;
-    String cryptoURL = "https://api.coinmarketcap.com/v1/ticker/?limit=${number}";
+    String cryptoURL = "https://api.coinmarketcap.com/v1/ticker/?limit=$number";
     http.Response response = await http.get(Uri.encodeFull(cryptoURL));
     return JSON.decode(response.body);
+  }
+
+  Widget getSubtitle(String priceUSD, String percentageChange){
+  
+    TextSpan priceTextWidget = new TextSpan(
+      text: "\$$priceUSD\n", // \$ is interpreted as $ character and the 2nd $ represents String interpolation
+      style: new TextStyle(color: Colors.black)
+    );
+    
+    String precentageChangeText = "1 hour: $percentageChange%";
+    
+    TextSpan percentageChangeWidget = double.parse(percentageChange)>0? new TextSpan(
+      text: precentageChangeText, style: new TextStyle(color: Colors.green)
+    ): new TextSpan(
+      text: precentageChangeText, style: new TextStyle(color: Colors.red)
+    );
+
+    return new RichText(
+      text: new TextSpan(
+        children: [priceTextWidget, percentageChangeWidget]
+      )
+    );
+  }
+
+  Widget getUI(Map currency, MaterialColor color){
+      return new ListTile(
+        leading: new CircleAvatar(
+          backgroundColor: color,
+          child: new Text(currency['name'][0]),
+          ),
+        title: new Text(currency['name'], style: new TextStyle(fontWeight: FontWeight.bold)),
+        subtitle: getSubtitle(currency['price_usd'], currency['percent_change_1h']),
+      );
   }
 
   Widget cryptoWidget(){
    return new Container(
      child: new Flexible(
        child: new ListView.builder(
+         itemCount: currencies.length,
          itemBuilder: (BuildContext context, int index){
+           
+           Map currency = currencies[index];
+           MaterialColor color = colors[index % colors.length];
+           return getUI(currency, color);
 
          },
        )
